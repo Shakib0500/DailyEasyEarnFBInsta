453db898ec15d36f61f2900a78210429e1f677c7
Shakib0500 <rsshakib07307@gmail.com>
2026-08-05 13:19:57 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     elif msg == "👥 Referral":
         await update.message.reply_text(
-            "👥 Your Referral Link:\nhttps://t.me/@FBInstaEasyEarnBot?start=" 
+            "👥 Your Referral Link:\nhttps://t.me/@FBInstaEasyEarnBot?start="
             + str(update.effective_user.id)
         )
 
@@ -32,3 +32,26 @@ async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     elif msg == "ℹ️ Help":
         await update.message.reply_text("Need help? Contact Admin.")
+
+
+async def admin_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == "ref_bonus":
+        await query.message.reply_text("👥 Current Referral Bonus: 0.01 USDT")
+
+    elif query.data == "task_reward":
+        await query.message.reply_text("🎯 Current Task Reward: 0.01 USDT")
+
+    elif query.data == "min_withdraw":
+        await query.message.reply_text("💸 Minimum Withdraw: 0.20 USDT")
+app = Application.builder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("admin", admin))
+app.add_handler(CallbackQueryHandler(admin_buttons))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, buttons))
+
+print("Bot is running...")
+app.run_polling()
