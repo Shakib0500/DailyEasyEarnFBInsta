e60adc3bf9d9233cfabbadd24b9e1378701b5a1a
Shakib0500 <rsshakib07307@gmail.com>
2026-08-05 18:25:17 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,8 +59,7 @@ async def buttons(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
     elif msg == "📋 Task":
-        await 
-update.message.reply_text(
+        await update.message.reply_text(
             f"📋 Task Reward: ${TASK_REWARD:.2f} USDT"
         )
 
